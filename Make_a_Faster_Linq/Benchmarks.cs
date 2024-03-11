22912db1e8fad6ff0e96b7e74a90aa22722bfeeb
Szücs Gábor Attila <gabor.attila.szucs@gmail.com>
2024-03-11 01:39:00 +0100

    SimdLinq test, benchmarking

---
+++ Make_a_Faster_Linq/Benchmarks.cs
@@ -0,0 +1,53 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Make_a_Faster_Linq
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        private static readonly int[] data = Enumerable.Range(0, 1000).ToArray();
+
+        [Benchmark]
+        public int ForLoop()
+        {
+            Span<int> _data = new Span<int>(data);
+            var length = _data.Length;
+            var sum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                sum += _data[i];
+            }
+            return sum;
+        }
+        [Benchmark]
+        public int SumLinq()
+        {
+            return data.Sum();
+        }
+        [Benchmark]
+        public int SimdLinq_()
+        {
+            return SimdLinq.SimdLinqExtensions.Sum(data);
+        }
+        [Benchmark]
+        public double AverageLinq()
+        {
+            return data.Average();
+        }
+        [Benchmark]
+        public double SimdLinqAverage_()
+        {
+            return SimdLinq.SimdLinqExtensions.Average(data);
+        }
+        [Benchmark]
+        public int WhereSumLinq()
+        {
+            return data.Where<int>(x=>x % 2 == 0).Sum();
+        }
+        [Benchmark]
+        public int SimdLinqWhere()
+        {
+            return SimdLinq.SimdLinqExtensions.Sum(data.Where<int>(x=>x % 2 == 0).ToArray<int>());
+        }
+    }
+}
---
+++ Make_a_Faster_Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Running;
+using SimdLinq;
+
+namespace Make_a_Faster_Linq
+{
+    /**
+     * We are using BenchmarkDotNet to run the benchmarks.
+     * BenchmarkDotNet will run the benchmarks and provide the results.
+     * We watch the difference of for,linq and SimdLinq nuget package.
+     * The SimdLinq nuget package is faster than linq.
+     * https://www.youtube.com/watch?v=5JKhNV9TY8k
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+
+            (int min, int max) GetMinMax(int[] data)
+            {
+                return data.MinMax();
+            }
+        }
+    }
+}
